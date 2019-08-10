9e640e6e3c4bf6792ec138beadc1005ef5eed4f8
Ihor Didyk <ihordidyk95@gmail.com>
2019-08-10 20:10:40 +0300

    created project

---
+++ src/main/java/io/pyxiscode/reminder/RemindmeServiceApplication.java
@@ -0,0 +1,13 @@
+package io.pyxiscode.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindmeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemindmeServiceApplication.class, args);
+    }
+
+}
