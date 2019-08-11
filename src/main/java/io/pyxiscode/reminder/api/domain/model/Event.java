a8a52d681dcb58b7673223c9f2edc7ef0cfa0aa0
Ihor Didyk <ihordidyk95@gmail.com>
2019-08-11 12:40:44 +0300

    resolved data mapping issue

--- src/main/java/io/pyxiscode/reminder/api/domain/model/Event.java
+++ src/main/java/io/pyxiscode/reminder/api/domain/model/Event.java
@@ -3,18 +3,23 @@ package io.pyxiscode.reminder.api.domain.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Document(collection = "events")
 @JsonIgnoreProperties(value = {"createAt"}, allowGetters = true)
-@Data
+@Getter
+@Setter
 public class Event {
 
     @Id
@@ -24,10 +29,10 @@ public class Event {
     @Size(max = 120, message = "Title should be less 120 characters")
     @Indexed(unique = true)
     private String title;
-    
+
     private Boolean completed = false;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH-mm")
-    private LocalDate createAt = LocalDate.now();
+    private LocalDateTime createAt = LocalDateTime.now();
 
 }
